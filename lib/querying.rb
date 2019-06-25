4d924abeeba683b9dd874c385820f6109ff3a83c
Abagail Hudson <abagailanne@gmail.com>
2019-06-25 00:07:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,9 +21,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN series
+  INNER JOIN series
   ON series.author_id = authors.id
-  JOIN subgenres
+  INNER JOIN subgenres
   ON series.subgenre_id = subgenres.id
   GROUP BY authors.name;"
 end
