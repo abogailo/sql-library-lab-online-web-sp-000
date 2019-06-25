8ea0c8d7caff388965fe6afc8d2f9f0e81b98783
Abagail Hudson <abagailanne@gmail.com>
2019-06-25 00:04:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year 
-  FROM books 
+  "SELECT title, year
+  FROM books
   WHERE series_id = 1
   ORDER by year;"
 end
@@ -19,10 +19,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
+  "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON series.author_id = authors.id 
+  ON series.author_id = authors.id
   JOIN subgenres
   ON series.subgenre_id = subgenres.id
   GROUP BY authors.name;"
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   JOIN character_books ON character_books.book_id = books.id
   JOIN characters ON characters.id = character_books.character_id
   WHERE characters.species = 'human'
-  GROUP BY series.title  
+  GROUP BY series.title
   ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
