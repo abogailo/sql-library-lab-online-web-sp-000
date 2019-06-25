57216cb9a0a6ee09820b1d1c17df0e9f9b3eedaa
Abagail Hudson <abagailanne@gmail.com>
2019-06-25 00:06:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,7 +42,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) as book_count
   FROM characters
-  JOIN character_books ON character_books.character_id = characters.id
+  INNER JOIN character_books ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY book_count DESC;"
 end
